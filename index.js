b5ec84b8a595cbff337d8b238d3f1735f535aec7
Lucas Bastos <lucas@lucasbastos.com>
2021-04-21 23:07:36 -0400

    Post request handling

--- index.js
+++ index.js
@@ -1,15 +1,32 @@
 const express = require('express');
 const http = require('http');
+const bodyParser = require('body-parser');
+const formidable = require('formidable');
 const app = express();
 const path = require('path');
 const port = process.env.PORT || 3000;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json())
 
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
 
-app.get('/callScript', function(req, res) {
-    res.end(`${req.body.file}`);
+app.post('/callScript', function(req, res) {
+    const form = new formidable.IncomingForm();
+
+    form.parse(req, function(err, fields, files) {
+        if (err != null) {
+            console.log(err)
+            return res.status(400).json({ message: err.message });
+        }
+
+        const [filename] = Object.keys(files);
+    });
+
+    res.send(req.body);
 });
+
 
 app.listen(port, () => console.log(`Listening on port http://localhost:${port}!`));
