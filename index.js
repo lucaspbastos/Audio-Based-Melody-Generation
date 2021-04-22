049a6faf003c3e18a5450f758bdf36e4cb4ff6fb
Lucas Bastos <lucas@lucasbastos.com>
2021-04-21 22:29:46 -0400

    Revert "Setting up https"
    This reverts commit 2f9c177ca46080d79cfa8aa6fbdfa6fe3af8c1ba.

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const port = process.env.PORT || 3000;
 
-var port = process.env['PORT'] = process.env.PORT || 4000;
-
-http.createServer(app).listen(port, function() {
-    console.log("Express server listening with http on port %d in %s mode", this.address().port, app.settings.env);
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
 });
+
+app.listen(port, () => console.log(`Listening on port http://localhost:${port}!`));
