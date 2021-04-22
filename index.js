6cc3f601473d75d5d2153e0e759f6d12d000dde0
Lucas Bastos <lucas@lucasbastos.com>
2021-04-21 22:06:12 -0400

    Adjust port for heroku

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
