da63642ef5ae3c4981e29299d86c0909751d1dc1
Lucas Bastos <lucas@lucasbastos.com>
2021-04-21 22:43:25 -0400

    Add new script page

--- index.js
+++ index.js
@@ -8,4 +8,8 @@ app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
 
+app.get('/callScript', function(req, res) {
+    res.send(`${req.body.file}`);
+});
+
 app.listen(port, () => console.log(`Listening on port http://localhost:${port}!`));
