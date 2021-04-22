c1f7e892f7a2ada366477d0a9b163516f22a3eaf
Lucas Bastos <lucas@lucasbastos.com>
2021-04-21 22:47:42 -0400

    Added response ending

--- index.js
+++ index.js
@@ -6,10 +6,11 @@ const port = process.env.PORT || 3000;
 
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
+    res.end();
 });
 
 app.get('/callScript', function(req, res) {
-    res.send(`${req.body.file}`);
+    res.end(`${req.body.file}`);
 });
 
 app.listen(port, () => console.log(`Listening on port http://localhost:${port}!`));
