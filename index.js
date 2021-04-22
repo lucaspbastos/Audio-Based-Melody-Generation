204fa74296b467895e3e761159f81c348a902714
Lucas Bastos <lucas@lucasbastos.com>
2021-04-21 22:03:45 -0400

    Set up front page

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const port = 3000;
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.listen(port, () => console.log(`Listening on port http://localhost:${port}!`));
