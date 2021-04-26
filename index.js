702a914b44a12765151095ba6360c39fe8e95c14
Lucas Bastos <lucas@lucasbastos.com>
2021-04-25 23:57:17 -0400

    Edit file dir

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.post('/callScript', function(req, res) {
 
     form.parse(req, function(err, fields, files) {
         var oldpath = files.file.path;
-        var newpath = 'uploads/' + files.file.name;
+        var newpath = './uploads/' + files.file.name;
         fs.rename(oldpath, newpath, function (err) {
             if (err) throw err;
             res.write('File uploaded and moved!');
